0c8a451bfd7f5ea1693df4068e7bd857db5fcb30
ttonmoy294-dev <ttonmoy294@gmail.com>
2025-12-15 14:24:29 +0900

    Update MINIMIZE.py

--- Codeforces/MINIMIZE.py
+++ Codeforces/MINIMIZE.py
@@ -1,5 +1,8 @@
-t = int(input())
-for x in range(t):
-    a,b = map(int,input().split())
-    c = abs((a-b))
-    print(c)
+# Codeforces Problem 2009A - Minimize!
+# https://codeforces.com/contest/2009/problem/A
+# Solved by トンモイ
+t = int(input())  # Number of test cases
+for _ in range(t):
+    a, b = map(int, input().split())  # Read two integers
+    c = abs(a - b)  # Calculate absolute difference
+    print(c)  # Print the result
